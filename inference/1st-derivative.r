3957a412e90ad0b05d2971ba5f3b3e99d137c6c5
yizenglistat <yizeng@email.sc.edu>
2020-01-04 16:55:44 -0500

    'finish 1st-derivative.r'

--- inference/1st-derivative.r
+++ inference/1st-derivative.r
@@ -1,31 +1,50 @@
-pp.d<- function(X,beta,delta,                             # target derivative var iables
-                alpha1,alpha2,ord,niknots,wrt='beta'){    # fixed parameters
-  u1 <- g(X%*%beta,alpha1,ord,niknots)        # individual risk probability for disease type 1
-  u2 <- g(X%*%beta,alpha2,ord,niknots)        # individual risk probability for disease type 2
-  SpDeriv1 <- SpDeriv(X%*%beta,alpha1,r,m)    # first derivative of spline function (individual risk scores) for disease type 1
-  SpDeriv2 <- SpDeriv(X%*%beta,alpha2,r,m)    # first derivative of spline function (individual risk scores) for disease type 2
-  
-  p11.db <- as.vector(-exp(-((-log(u1))^(1/delta)+(-log(u2))^(1/delta))^delta)*
-                       ((-log(u1))^(1/delta)+(-log(u2))^(1/delta))^(delta-1)*
-                       ((-log(u1))^(1/delta)/(log(u1))*(1-u1)*SpDeriv1
-                         +(-log(u2))^(1/delta)/(log(u2))*(1-u2)*SpDeriv2))*X
+"
+This function computes first-order derivative of risk probability matrix with respect to beta (or delta)
 
-  p11.dr<- -((-log(u2))^(1/delta)+(-log(u1))^(1/delta))^delta*
+Input:
+  
+  X      : features matrix
+  beta    : feature coefficients
+  delta   : gumbel parameter
+  alpha1  : spline coefficients
+  alpha2  : spline coefficients
+  ord     : order of spline
+  niknots : number of interior knots
+  wrt     : derivative with respect to which variable, 'both' or 'beta'
+
+Output:
+  derivative matrix for beta (or delta)
+"
+pmatrix_d<- function(X,beta,delta,alpha1,alpha2,ord,niknots,wrt='both'){
+  # individual risk probability for disease type 1/type 2
+  u1 <- g(X%*%beta,alpha1,ord,niknots)
+  u2 <- g(X%*%beta,alpha2,ord,niknots)
+  # first derivative of spline function (individual risk scores) for disease type 1/type 2    
+  sp_d1 <- sp_d(X%*%beta,alpha1,ord,niknots)   
+  sp_d2 <- sp_d(X%*%beta,alpha2,ord,niknots)
+  # first derivative of probability of having two diseases simutaneously wrt beta
+  p11_dbeta <- as.vector(-exp(-((-log(u1))^(1/delta)+(-log(u2))^(1/delta))^delta)*
+                       ((-log(u1))^(1/delta)+(-log(u2))^(1/delta))^(delta-1)*
+                       ((-log(u1))^(1/delta)/(log(u1))*(1-u1)*sp_d1
+                         +(-log(u2))^(1/delta)/(log(u2))*(1-u2)*sp_d2))*X
+  # first derivative of probability of having two diseases simutaneously wrt delta
+  p11_ddelta<- -((-log(u2))^(1/delta)+(-log(u1))^(1/delta))^delta*
     ((-(-log(u2))^(1/delta)*log(-log(u2))/delta^2-(-log(u1))^(1/delta)*
         log(-log(u1))/delta^2)*delta/((-log(u2))^(1/delta)+(-log(u1))^(1/delta))+
        log((-log(u2))^(1/delta)+(-log(u1))^(1/delta)))*exp(-((-log(u2))^(1/delta)+(-log(u1))^(1/delta))^delta)
-<script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
-  if(dim == p+1){ # dim == p+1
-    p11.d<- cbind(p11.db,p11.dr)
-    p10.d<- cbind(as.vector(u1*(1-u1)*SpDeriv1)*X,matrix(0,N,1))-p11.d
-    p01.d<- cbind(as.vector(u2*(1-u2)*SpDeriv2)*X,matrix(0,N,1))-p11.d
-    p00.d<- -p11.d-p10.d-p01.d
+  # first derivative of risk probability matrix wrt beta and delta
+  if(wrt=='both'){
+    p11_d<- cbind(p11_dbeta,p11_ddelta)
+    p10_d<- cbind(as.vector(u1*(1-u1)*sp_d1)*X, matrix(0,N,1))-p11_d
+    p01_d<- cbind(as.vector(u2*(1-u2)*sp_d2)*X, matrix(0,N,1))-p11_d
+    p00_d<- -p11_d-p10_d-p01_d
   }
-  if(dim == p){ # dim == p
-    p11.d<- p11.db
-    p10.d<- as.vector(u1*(1-u1)*SpDeriv1)*X-p11.d
-    p01.d<- as.vector(u2*(1-u2)*SpDeriv2)*X-p11.d
-    p00.d<- -p11.d-p10.d-p01.d
+  # first derivative of risk probability matrix wrt beta
+  if(wrt=='beta'){
+    p11_d<- p11_dbeta
+    p10_d<- as.vector(u1*(1-u1)*sp_d1)*X-p11_d
+    p01_d<- as.vector(u2*(1-u2)*sp_d2)*X-p11_d
+    p00_d<- -p11_d-p10_d-p01_d
   }
-  return(list(p00.d=p00.d,p10.d=p10.d,p01.d=p01.d,p11.d=p11.d))
+  return(list(p00_d=p00_d,p10_d=p10_d,p01_d=p01_d,p11_d=p11_d))
 }
